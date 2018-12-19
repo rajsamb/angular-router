f952eafad69184c66aa557ef55207189ca4b42f4
sambhu.singh <sambhu.singh@L443MALT.local>
2018-12-19 12:58:00 +0000

    [FEATURE] - animation module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HeroesModule }     from './heroes/heroes.module';
 
 import { CrisisListComponent }   from './crisis-list/crisis-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        BrowserAnimationsModule,
         HeroesModule,
         AppRoutingModule
     ],
